e1a201a1ba27656731876a373e1c35105243e993
Nebury <holasoyluis1998@gmail.com>
2019-05-03 04:45:08 -0600

    Ventana automatica

--- src/app/Components/family/family.component.ts
+++ src/app/Components/family/family.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-family',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./family.component.css']
 })
 export class FamilyComponent implements OnInit {
-
+  @ViewChild('openModal') openModal:ElementRef;
   constructor() { }
 
   ngOnInit() {
+    this.openModal.nativeElement.click();
   }
 
 }
